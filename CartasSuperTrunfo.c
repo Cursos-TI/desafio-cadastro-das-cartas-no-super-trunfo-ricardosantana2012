e94934f845cdaf2de34a5cdec2786eef56448f05
ricardosantana2012 <ricardosantana2012@hotmail.com>
2025-03-21 20:11:38 +0000

    adiconado meu interativo / for para compara exeuctar mais de 1 vez

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,11 +1,22 @@
 #include <stdio.h>
 #include <string.h>
 
+// Função para capturar o nome string com espaço
+void capturarNome(char *nome, int tamanho) {
+    getchar(); // Limpa o buffer para evitar problemas com fgets
+    fgets(nome, tamanho, stdin); // Lê a string com espaços
+
+    // Remove o '\n' caso esteja presente
+    nome[strcspn(nome, "\n")] = '\0';
+}
+
+
 int main(){
 
-    int pontosTur1,pontosTur2,opcao;
+    int pontosTur1,pontosTur2,opcao1,opcao2;
     unsigned long int populacao1,populacao2;
-    float area1,area2,pib1,pib2,pibpercapita1,pibpercapita2,denpopulacional1,denpopulacional2,superpodercarta1, superpodercarta2;
+    float area1,area2,pib1,pib2,pibpercapita1,pibpercapita2,denpopulacional1,
+    denpopulacional2,superpodercarta1, superpodercarta2,somaAtributo1,somaAtributo2;
     char estado1[4],estado2[4];
     char cidade1[30],cidade2[30],codc1[4],codc2[4],pais1[30],pais2[30];
     
@@ -15,22 +26,18 @@ int main(){
     //leitura de dados carta 1
     printf("Digite as informações da carta 1: \n");
    
-    printf("Digite a sigla do Estado: "); 
-    scanf(" %s", &estado1);
+    printf("Digite o codigo da carta: ");
+    scanf(" %s", &codc1);  
     
     printf("Digite o nome do pais: ");
-    getchar(); //lipar \n do enter para fgets funcionar
-    fgets(pais1, sizeof(pais1), stdin); // ler string com espaço
-    pais1[strcspn(pais1, "\n")] = '\0'; //remove a quebra de linha
+    capturarNome(pais1, sizeof(pais1));
+
+    //printf("Digite a sigla do Estado: "); 
+    //scanf(" %s", &estado1);
     
-    printf("Digite o codigo da carta: ");
-    scanf(" %s", &codc1);
-    
-    printf("Digite o nome da Cidade: ");     
-    getchar(); //lipar \n do enter para fgets funcionar
-    fgets(cidade1, sizeof(cidade1), stdin); // ler string com espaço
-    cidade1[strcspn(cidade1, "\n")] = '\0'; //remove a quebra de linha
-   
+    //printf("Digite o nome da Cidade: "); 
+    //capturarNome(cidade1, sizeof(cidade1));
+       
     printf("Digite o numero de habitantes: ");
     scanf("%u",&populacao1);
     printf("Digite a area em km2: ");
@@ -42,24 +49,19 @@ int main(){
 
     //leitura de dados carta 2
     printf("\nDigite as informações da carta 2: \n");
-   
-    printf("Digite a sigla do Estado: "); 
-    scanf(" %s", &estado2);
-   
-    printf("Digite o nome do pais: ");
-    getchar(); //lipar \n do enter para fgets funcionar
-    fgets(pais2, sizeof(pais2), stdin); // ler string com espaço
-    pais2[strcspn(pais2, "\n")] = '\0'; //remove a quebra de linha
-   
+          
     printf("Digite o codigo da carta: ");
-    scanf(" %s", &codc2);
+    scanf(" %s", &codc2);  
     
-    printf("Digite o nome da Cidade: ");        
-    getchar(); //lipar \n do enter para fgets funcionar
-    fgets(cidade2, sizeof(cidade2), stdin); // ler string com espaço
-    cidade2[strcspn(cidade2, "\n")] = '\0'; //remove a quebra de linha
+    printf("Digite o nome do pais: ");
+    capturarNome(pais2,sizeof(pais2));
 
-
+   //printf("Digite a sigla do Estado: "); 
+    //scanf(" %s", &estado2);
+   
+    //printf("Digite o nome da Cidade: ");        
+    //capturarNome(cidade2, sizeof(cidade2));
+  
     printf("Digite o numero de habitantes: ");
     scanf("%u",&populacao2);
     printf("Digite a area em km2: ");
@@ -82,11 +84,10 @@ int main(){
     superpodercarta2 = (float) populacao2+area2+pib2+pontosTur2+pibpercapita2+(area2/populacao2);
 
     //escrita de resultados cicade 1
-    printf("\nCarta 1: \n");
-    printf("Estado: %s\n",estado1);
-    printf("Codigo: %s\n",codc1);
-    printf("Nome da Cidade: %s\n",cidade1);
+    printf("\nCarta %s: \n",codc1);
     printf("Nome do Pais: %s\n",pais1);
+    //printf("Estado: %s\n",estado1);
+   //printf("Nome da Cidade: %s\n",cidade1);        
     printf("Populacao: %u\n",populacao1);
     printf("Area: %.2f km²\n",area1);
     printf("PIB: %.2f bilhões de reais\n",pib1);
@@ -96,11 +97,10 @@ int main(){
     printf("Super poder: %.2f\n",superpodercarta1);
 
     //escrita de resultados cicade 2
-    printf("\nCarta 2: \n");
-    printf("Estado: %s\n",estado2);
-    printf("Codigo: %s\n",codc2);
-    printf("Nome da Cidade: %s\n",cidade2);
+    printf("\nCarta %s: \n",codc2);
     printf("Nome do Pais: %s\n",pais2);
+   //printf("Estado: %s\n",estado2);
+   //printf("Nome da Cidade: %s\n",cidade2);
     printf("Populacao: %u\n",populacao2);
     printf("Area: %.2f km²\n",area2);
     printf("PIB: %.2f bilhões de reais\n",pib2);
@@ -112,7 +112,7 @@ int main(){
 
 
     //menu de escolha usuario       
-    printf("\n### Escolha Uma opção ###\n");
+    printf("\n### Escolha Uma a primeira opção ###\n");
     printf("\n1. Nome dos Paises:\n");
     printf("2. População\n");
     printf("3. Área\n");
@@ -120,77 +120,162 @@ int main(){
     printf("5. Numero de pontos turisticos\n");
     printf("6. Densidade Demografica\n");
     printf("Escolha uma opção: ");
-    scanf("%d", &opcao);
+    scanf("%d", &opcao1);
+
+    if(opcao1>0 && opcao1<7 ){
+    
+    //case switch para alterar menu
+    switch (opcao1)
+    {
+        case 2:
+        printf("\n### Escolha Uma a segunda comparação ###\n");
+        printf("3. Área\n");
+        printf("4. PIB\n");
+        printf("5. Numero de pontos turisticos\n");
+        printf("6. Densidade Demografica\n");
+        printf("Escolha uma opção: ");
+        scanf("%d", &opcao2);
+        break;
+        case 3:
+        printf("\n### Escolha Uma a segunda comparação ###\n");
+        printf("2. População\n");
+        printf("4. PIB\n");
+        printf("5. Numero de pontos turisticos\n");
+        printf("6. Densidade Demografica\n");
+        printf("Escolha uma opção: ");
+        scanf("%d", &opcao2);
+
+        break;
+        case 4:
+        printf("\n### Escolha Uma a segunda comparação ###\n");
+        printf("2. População\n");
+        printf("3. Área\n");
+        printf("5. Numero de pontos turisticos\n");
+        printf("6. Densidade Demografica\n");
+        printf("Escolha uma opção: ");
+        scanf("%d", &opcao2);
+        break;
+        case 5:
+        printf("\n### Escolha Uma a segunda comparação ###\n");
+        printf("2. População\n");
+        printf("3. Área\n");
+        printf("4. PIB\n");
+        printf("6. Densidade Demografica\n");
+        printf("Escolha uma opção: ");
+        scanf("%d", &opcao2);
+        break;
+        case 6:
+        printf("\n### Escolha Uma a segunda comparação ###\n");
+        printf("2. População\n");
+        printf("3. Área\n");
+        printf("4. PIB\n");
+        printf("5. Numero de pontos turisticos\n");
+        printf("Escolha uma opção: ");
+        scanf("%d", &opcao2);
+        break;
+
+    
+    default:
+        break;
+    }
+
+
 
 
     //switch para escolha e resultado ds comparações
-    switch (opcao){
+    for(int contador = 0; contador<=1; contador++){
+    switch (opcao1){
     case 1:
-        printf("\nCarta 1 - Nome do Pais: %s",cidade1);
-        printf("\nCarta 2 - Nome da Pais: %s",cidade2);
+        printf("\nCarta 1 - Nome do Pais: %s",pais1);
+        printf("\nCarta 2 - Nome da Pais: %s",pais2);
     break;
     case 2:        
+        opcao1=opcao2;
+        somaAtributo1=somaAtributo1+populacao1;
+        somaAtributo2=somaAtributo2+populacao2;           
         printf("\nComparação de cartas (Atributo: População):\n");
         printf("\nCarta 1 - Pais %s tem uma população de: %d",pais1,populacao1);
         printf("\nCarta 2 - Pais %s tem uma população de: %d",pais2,populacao2);
-        if(populacao1>pontosTur2){
-            printf("\nResultado: Carta 1 venceu!");
-        }else if(populacao1<pontosTur2){
-            printf("\nResultado: Carta 2 venceu!");
+        if(populacao1>populacao2){
+            printf("\nResultado: Carta 1 venceu!\n");
+        }else if(pontosTur1<populacao2){
+            printf("\nResultado: Carta 2 venceu!\n");
         }else{
-            printf("\nEmpate entre os atributos");
-        }        
+            printf("\nEmpate entre os atributos\n");
+        }                    
     break;
     case 3:
+        opcao1=opcao2; 
+        somaAtributo1=somaAtributo1+area1;
+        somaAtributo2=somaAtributo2+area2;
         printf("\nComparação de cartas (Atributo: Área):\n");
         printf("\nCarta 1 - Pais %s tem uma Área de: %.2f",pais1,area1);
         printf("\nCarta 2 - Pais %s tem uma Área de: %.2f",pais2,area2);
         if(area1>area2){
-            printf("\nResultado: Carta 1 venceu!");
+            printf("\nResultado: Carta 1 venceu!\n");
         }else if(area1<area2){
-            printf("\nResultado: Carta 2 venceu!");
+            printf("\nResultado: Carta 2 venceu!\n");
         }else{
-            printf("\nEmpate entre os atributos");
+            printf("\nEmpate entre os atributos\n");
         } 
     break;
     case 4:
-        printf("\nComparação de cartas (Atributo: PIB:\n");
+        somaAtributo1=somaAtributo1+pib1;
+        somaAtributo2=somaAtributo2+pib2;
+        opcao1=opcao2; 
+        printf("\nComparação de cartas (Atributo: PIB):\n");
         printf("\nCarta 1 - Pais %s tem um PIB de: %.2f",pais1,pib1);
         printf("\nCarta 2 - Pais %s tem um PIB de: %.2f",pais2,pib2);
         if(pib1>pib2){
-            printf("\nResultado: Carta 1 venceu!");
+            printf("\nResultado: Carta 1 venceu!\n");
         }else if(pib1<pib2){
-            printf("\nResultado: Carta 2 venceu!");
+            printf("\nResultado: Carta 2 venceu!\n");
         }else{
-            printf("\nEmpate entre os atributos");
+            printf("\nEmpate entre os atributos\n");
         } 
     break;
     case 5:
-        printf("\nComparação de cartas (Atributo: Pontos Turisticos:\n");
+        opcao1=opcao2;
+        somaAtributo1=somaAtributo1+pontosTur1;
+        somaAtributo2=somaAtributo2+pontosTur2; 
+        printf("\nComparação de cartas (Atributo: Pontos Turisticos):\n");
         printf("\nCarta 1 - Pais %s tem %d pontos turisticos",pais1,pontosTur1);
         printf("\nCarta 2 - Pais %s tem %d pontos turisticos",pais2,pontosTur2);
         if(pontosTur1>pontosTur2){
-            printf("\nResultado: Carta 1 venceu!");
+            printf("\nResultado: Carta 1 venceu!\n");
         }else if(pontosTur1<pontosTur2){
-            printf("\nResultado: Carta 2 venceu!");
+            printf("\nResultado: Carta 2 venceu!\n");
         }else{
-            printf("\nEmpate entre os atributos");
+            printf("\nEmpate entre os atributos\n");
         }             
     break;
     case 6:
-        printf("\nComparação de cartas (Atributo: Densidade demográfica:\n");
+        somaAtributo1=somaAtributo1-denpopulacional1;
+        somaAtributo2=somaAtributo2-denpopulacional2;
+        opcao1=opcao2; 
+        printf("\nComparação de cartas (Atributo: Densidade demográfica):\n");
         printf("\nCarta 1 - Pais %s tem uma Densidade demográfica de: %.2f",pais1,denpopulacional1);
         printf("\nCarta 2 - Pais %s tem uma Densidade demográfica de: %.2f",pais2,denpopulacional2);
         if(denpopulacional2>denpopulacional1){
-            printf("\nResultado: Carta 1 venceu!");
+            printf("\nResultado: Carta 1 venceu!\n");
         }else if(denpopulacional2<denpopulacional1){
             printf("\nResultado: Carta 2 venceu!");
         }else{
-            printf("\nEmpate entre os atributos");
+            printf("\nEmpate entre os atributos\n");
         } 
     break;
-    default:
-    printf("Opção inválida. Tente novamente.\n");
     }
-    
+}
+//exibir resultados de somas de atibutos escolhidos    
+printf("\nComparação de cartas (Atributo: Soma de atributos escolhidos):\n");
+printf("\nCarta 1 - Pais %s tem uma uma soma total de: %.2f",pais1,somaAtributo1);
+printf("\nCarta 2 - Pais %s tem uma uma soma total de: %.2f\n",pais2,somaAtributo2);
+if(somaAtributo1==somaAtributo2){
+    printf("Empate!!!");
+}else{
+somaAtributo1>somaAtributo2 ? printf("Carta 1 venceu!") : printf("carta 2 venceu");
+}
+}else{
+printf("Opção inválida. Tente novamente.\n");
+}
 }
